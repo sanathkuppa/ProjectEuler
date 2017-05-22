66a23c5cf2732532909420c2b4f1173ef712bf85
Sanath Kuppa <sanathkuppa@gmail.com>
2017-05-22 17:25:44 -0400

    Create Problem_69.r

---
+++ Problem_69.r
@@ -0,0 +1,4 @@
+# For n/phi(n) to be maximum, we have to minimize phi(n)
+# phi(n) is small when it is relatively prime to less numbers
+# This happens when n has many distinct prime factors 
+# The largest number less than 1000000 is 2*3*5*7*11*13*17 = 510510
